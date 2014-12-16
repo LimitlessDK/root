ed2800b6639176a5c3a260b1fc6cea0b38863a22
EMA <ema@company.local>
2014-12-16 16:56:49 +0100

    A1 Main 3

--- A1Project/src/MainClass.java
+++ A1Project/src/MainClass.java
@@ -3,7 +3,7 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("A1Project Main 2");
+		System.out.println("A1Project Main 3");
 	}
 
 }
